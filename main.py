b7a3380688d3e786272e6453ff21be9358cc2f03
olo00olo <olo00olo@protonmail.com>
2022-10-20 16:34:35 +0200

    side menu

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
 import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QPushButton
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
 
-def window():
-    app = QApplication(sys.argv)
-    window = QtWidgets.QWidget()
-    window.setWindowTitle("PetriPy")
-
-    button1 = QPushButton(window)
-    button1.setText("Button1")
-    button1.setCheckable(True)
-    button1.move(64, 32)
-    button1.setGeometry(200, 150, 200, 200)
-    button1.setStyleSheet("border: 10px solid blue; border-radius: 100px")
-    button1.clicked.connect(button1_clicked)
+    newPlace = QPushButton("0")
+    newPlace.clicked.connect(lambda: dock.show())
+    newPlace.show()
 
 
-    button2 = QPushButton(window)
-    button2.setText("Button2")
-    button2.move(64, 128)
-    button2.clicked.connect(button2_clicked)
 
-    window.setGeometry(50, 50, 1000, 1000)
-    window.show()
+    w = QtWidgets.QMainWindow()
+    w.setCentralWidget(QtWidgets.QWidget())
+    dock = QtWidgets.QDockWidget("Collapsible Demo")
+    w.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+
+
+
+
+    scroll = QtWidgets.QScrollArea()
+    dock.setWidget(scroll)
+
+
+
+    # print(dock.isActiveWindow(), dock.isHidden(), dock.isEnabled())
+    #
+    #
+    # newPlace = QPushButton("0")
+    # newPlace.clicked.connect(lambda: dock.show())
+    # newPlace.show()
+
+    # content = QtWidgets.QWidget()
+    # scroll.setWidget(content)
+    # vlay = QtWidgets.QVBoxLayout(content)
+    # vlay.addStretch()
+
+    # w.resize(640, 480)
+    w.show()
     sys.exit(app.exec_())
-
-
-def button1_clicked():
-    print("Button 1 clicked")
-
-
-
-def button2_clicked():
-    print("Button 2 clicked")
-    newBtn = QPushButton()
-    newBtn.setText("New button")
-    newBtn.move(0, 0)
-    newBtn.show()
-
-
-if __name__ == '__main__':
-    window()
-
-
--- main2.py
+++ main2.py
@@ -1,13 +1,34 @@
+# coffee counter = 2
+# hours spend = about 5
 import sys
 
-from PyQt6.QtWidgets import QApplication, QDialog, QPushButton
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QPushButton, QMenu, QDockWidget, QMainWindow, QWidget, QScrollArea, QLabel, \
+    QVBoxLayout, QTextEdit
+from PyQt6.uic.properties import QtCore
 
 
-class Window(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(Window, self).__init__(parent=None)
-
+        super(MainWindow, self).__init__(parent=None)
         self.button_map = []
+
+        self.setCentralWidget(QWidget())
+        self.dock = QDockWidget("Menu")
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.dock)
+        scroll = QScrollArea()
+        self.dock.setWidget(scroll)
+
+        self.text = QLabel("Text")
+        self.text.setDisabled(True)
+        wid = QWidget()
+        lay = QVBoxLayout(wid)
+        lay.addWidget(self.text)
+        self.dock.setWidget(wid)
+        self.dock.setVisible(False)
+
+
+
 
         self.setWindowTitle("PetriPy")
         self.resize(600, 600)
@@ -16,21 +37,23 @@ class Window(QDialog):
         self.addPlaceBtn.move(5, 5)
         self.addPlaceBtn.setCheckable(True)
 
-        self.addTransitionBtn = QPushButton("add transition", self)
-        self.addTransitionBtn.move(100, 5)
-        self.addTransitionBtn.setCheckable(True)
+        # self.addTransitionBtn = QPushButton("add transition", self)
+        # self.addTransitionBtn.move(100, 5)
+        # self.addTransitionBtn.setCheckable(True)
 
 
         self.changeLabelBtn = QPushButton("+1", self)
         self.changeLabelBtn.move(195, 5)
         self.changeLabelBtn.clicked.connect(self.change)
 
+
+
+
+
     def change(self):
         for place in self.button_map:
             if place.isChecked():
-                a = str(int(place.text())+1)
-                print(a)
-                place.setText(a)
+                place.setText(str(int(place.text())+1))
 
     def changeColorIfChecked(self, name):
         for place in self.button_map:
@@ -39,13 +62,26 @@ class Window(QDialog):
                 place.setChecked(False)
 
         if name.isChecked():
+            self.text.setText(str(name))
+            self.dock.setVisible(True)
             name.setStyleSheet("border: 3px solid darkblue; border-radius: 25px")
         else:
+            self.dock.setVisible(False)
             name.setStyleSheet("border: 3px solid dodgerblue; border-radius: 25px")
-        print(name.isChecked())
+        # print(name.isChecked())
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        delete = contextMenu.addAction("Delete")
+        action = contextMenu.exec(self.mapToGlobal(event.pos()))
+
 
     def mousePressEvent(self, QMouseEvent):
-        if self.addPlaceBtn.isChecked():
+        if QMouseEvent.button() == Qt.MouseButton.RightButton:
+            print(QMouseEvent.pos().x(), QMouseEvent.pos().y())
+
+
+        if self.addPlaceBtn.isChecked() and QMouseEvent.button() == Qt.MouseButton.LeftButton:
             newPlace = QPushButton("0", self)
             newPlace.setGeometry(QMouseEvent.pos().x() - 25, QMouseEvent.pos().y() - 25, 50, 50)
             newPlace.setStyleSheet("border: 3px solid dodgerblue; border-radius: 25px")
@@ -56,15 +92,15 @@ class Window(QDialog):
             self.button_map.append(newPlace)
             # print(self.button_map)
 
-        if self.addTransitionBtn.isChecked():
-            newPlace = QPushButton("0", self)
-            newPlace.setGeometry(QMouseEvent.pos().x() - 25, QMouseEvent.pos().y() - 25, 50, 50)
-            newPlace.setStyleSheet("border: 3px solid dodgerblue;")
-            newPlace.setCheckable(True)
-            newPlace.show()
+        # if self.addTransitionBtn.isChecked():
+        #     newPlace = QPushButton("0", self)
+        #     newPlace.setGeometry(QMouseEvent.pos().x() - 25, QMouseEvent.pos().y() - 25, 50, 50)
+        #     newPlace.setStyleSheet("border: 3px solid dodgerblue;")
+        #     newPlace.setCheckable(True)
+        #     newPlace.show()
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = Window()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
