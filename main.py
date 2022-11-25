0ca2f7fd44a6beba297fff642549dd14ceab4a4a
olo00olo <olo00olo@protonmail.com>
2022-11-25 14:39:28 +0100

    place class working

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import math
 
 from PyQt5.QtCore import (QLineF, QPointF, QRectF, QSizeF, Qt)
 from PyQt5.QtGui import (QBrush, QColor, QPainter,
-                         QPainterPath, QPen, QPolygonF, QRadialGradient, QFont)
+                         QPainterPath, QPen, QPolygonF, QRadialGradient)
 from PyQt5.QtWidgets import (QApplication, QGraphicsItem, QGraphicsScene,
-                             QGraphicsView, QStyle, QPushButton, QGraphicsSimpleTextItem, QGraphicsTextItem)
+                             QGraphicsView, QStyle, QPushButton, QGraphicsTextItem)
 
 
 class Edge(QGraphicsItem):
@@ -119,23 +119,24 @@ class Node(QGraphicsItem):
         self.edgeList = []
         self.newPos = QPointF()
 
+        self.label = None
+        self.tokens = None
+
         self.setFlag(QGraphicsItem.ItemIsMovable)
         self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
         self.setCacheMode(QGraphicsItem.DeviceCoordinateCache)
         self.setZValue(1)
 
-#TODO: scaling and moving
-        f = QFont()
-        f.setPointSize(4)
-
-        self.tokens = QGraphicsTextItem("0", self)
-        self.tokens.setPos(-7, -11)
-        self.tokens.setFont(f)
-
+    # TODO: scaling and moving
+    def labels(self):
         self.label = QGraphicsTextItem("P1", self)
         self.label.setPos(-20, -25)
-        self.label.setFont(f)
 
+    def token(self):
+        self.tokens = QGraphicsTextItem("0", self)
+        self.tokens.setPos(-7, -11)
+
+    @staticmethod
     def type(self):
         return Node.Type
 
@@ -157,6 +158,50 @@ class Node(QGraphicsItem):
         adjust = 2.0
         return QRectF(-10 - adjust, -10 - adjust, 23 + adjust, 23 + adjust)
 
+    # def shape(self):
+    #     path = QPainterPath()
+    #     path.addEllipse(-10, -10, 20, 20)
+    #     return path
+    #
+    # def paint(self, painter, option, widget):
+    #     painter.setPen(Qt.NoPen)
+    #     painter.drawEllipse(-7, -7, 20, 20)
+    #     gradient = QRadialGradient(-3, -3, 10)
+    #     if option.state & QStyle.State_Sunken:
+    #         gradient.setCenter(3, 3)
+    #         gradient.setFocalPoint(3, 3)
+    #         gradient.setColorAt(0, QColor(Qt.darkYellow).lighter(120))
+    #     else:
+    #         gradient.setColorAt(0, Qt.yellow)
+    #
+    #     painter.setBrush(QBrush(gradient))
+    #     painter.setPen(QPen(Qt.black, 0))
+    #     painter.drawEllipse(-10, -10, 20, 20)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionHasChanged:
+            for edge in self.edgeList:
+                edge.adjust()
+
+        return super(Node, self).itemChange(change, value)
+
+    def mousePressEvent(self, event):
+        self.update()
+        super(Node, self).mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        self.update()
+        super(Node, self).mouseReleaseEvent(event)
+
+
+class Place(Node):
+    Type = QGraphicsItem.UserType + 1
+
+    def __init__(self, graphWidget):
+        super().__init__(graphWidget)
+        self.labels()
+        self.token()
+
     def shape(self):
         path = QPainterPath()
         path.addEllipse(-10, -10, 20, 20)
@@ -172,51 +217,20 @@ class Node(QGraphicsItem):
             gradient.setFocalPoint(3, 3)
             gradient.setColorAt(0, QColor(Qt.darkYellow).lighter(120))
         else:
-            gradient.setColorAt(0, Qt.yellow)
+            gradient.setColorAt(0, Qt.red)
 
         painter.setBrush(QBrush(gradient))
         painter.setPen(QPen(Qt.black, 0))
         painter.drawEllipse(-10, -10, 20, 20)
-
-    def itemChange(self, change, value):
-        if change == QGraphicsItem.ItemPositionHasChanged:
-            for edge in self.edgeList:
-                edge.adjust()
-            self.graph.itemMoved()
-
-        return super(Node, self).itemChange(change, value)
-
-    def mousePressEvent(self, event):
-        self.update()
-        super(Node, self).mousePressEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        self.update()
-        super(Node, self).mouseReleaseEvent(event)
-
-class Place(Node):
-    Type = QGraphicsItem.UserType + 1
-
-    def __init__(self, graphWidget):
-        super(Node, self).__init__()
-
-        self.graph = graphWidget
-        self.edgeList = []
-        self.newPos = QPointF()
-
-
 
 
 class GraphWidget(QGraphicsView):
     def __init__(self):
         super(GraphWidget, self).__init__()
 
-        self.timerId = 0
-
         self.scene = QGraphicsScene(self)
         self.scene.setItemIndexMethod(QGraphicsScene.NoIndex)
         self.scene.setSceneRect(-200, -200, 600, 600)
-
 
         self.setScene(self.scene)
         self.setCacheMode(QGraphicsView.CacheBackground)
@@ -229,53 +243,15 @@ class GraphWidget(QGraphicsView):
         self.addPlaceBtn.move(5, 5)
         self.addPlaceBtn.setCheckable(True)
 
-        node1 = Node(self)
-        node2 = Node(self)
-        node3 = Node(self)
-        node4 = Node(self)
-        self.centerNode = Node(self)
-        node6 = Node(self)
-        node7 = Node(self)
-        node8 = Node(self)
-        node9 = Node(self)
-
-        node100 = Place(self)
-        self.scene.addItem(node100)
-        node100.setPos(200, 200)
-
+        node1 = Place(self)
         self.scene.addItem(node1)
+        node1.setPos(0, 0)
+
+        node2 = Place(self)
         self.scene.addItem(node2)
-        self.scene.addItem(node3)
-        self.scene.addItem(node4)
-        self.scene.addItem(self.centerNode)
-        self.scene.addItem(node6)
-        self.scene.addItem(node7)
-        self.scene.addItem(node8)
-        self.scene.addItem(node9)
+        node2.setPos(50, 50)
+
         self.scene.addItem(Edge(node1, node2))
-        self.scene.addItem(Edge(node2, node3))
-        self.scene.addItem(Edge(node2, self.centerNode))
-        self.scene.addItem(Edge(node3, node6))
-        self.scene.addItem(Edge(node4, node1))
-        self.scene.addItem(Edge(node4, self.centerNode))
-        self.scene.addItem(Edge(self.centerNode, node6))
-        self.scene.addItem(Edge(self.centerNode, node8))
-        self.scene.addItem(Edge(node6, node9))
-        self.scene.addItem(Edge(node7, node4))
-        self.scene.addItem(Edge(node8, node7))
-        self.scene.addItem(Edge(node9, node8))
-
-        node1.setPos(-50, -50)
-        node2.setPos(0, -50)
-        node3.setPos(50, -50)
-        node4.setPos(-50, 0)
-        self.centerNode.setPos(0, 0)
-        node6.setPos(50, 0)
-        node7.setPos(-50, 50)
-        node8.setPos(0, 50)
-        node9.setPos(50, 50)
-
-        self.p = node1.scenePos()
 
         self.scale(1.8, 1.8)
         self.setMinimumSize(400, 400)
@@ -285,24 +261,9 @@ class GraphWidget(QGraphicsView):
         self.panStartX = None
         self.panStartY = None
 
-    def showText(self):
-        node10 = Node(self)
-        self.scene.addItem(node10)
-        node11 = Node(self)
-        self.scene.addItem(node11)
-
-
-        self.scene.addItem(Edge(node10, node11))
-        node10.setPos(150, 150)
-        node11.setPos(250, 250)
-        print("ASD")
-
-    def itemMoved(self):
-        if not self.timerId:
-            self.timerId = self.startTimer(int(1000 / 25))
-
     def keyPressEvent(self, event):
         key = event.key()
+        print(key)
 
     def mousePressEvent(self, event):
         items = self.items(event.pos())
@@ -316,43 +277,42 @@ class GraphWidget(QGraphicsView):
                 newPlace.setPos(self.mapToScene(event.pos()))
                 self.scene.addItem(newPlace)
 
-        elif event.button() == Qt.MouseButton.RightButton:
-            self.rightButtonPressed = True
-            self.panStartX = event.x()
-            self.panStartY = event.y()
-            self.setCursor(Qt.ClosedHandCursor);
-            event.accept()
-
-        elif event.button() == Qt.MouseButton.MiddleButton:
-            self.centerOn(event.pos())
-
-    def mouseMoveEvent(self, event):
-        items = self.items(event.pos())
-        if items and isinstance(items[0], QGraphicsItem):
-            QGraphicsView.mouseMoveEvent(self, event)
-            return
-        if self.rightButtonPressed:
-            self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - (event.x() - self.panStartX))
-            self.verticalScrollBar().setValue(self.verticalScrollBar().value() - (event.y() - self.panStartY))
-            self.panStartX = event.x()
-            self.panStartY = event.y()
-            event.accept()
-
-
-    def mouseReleaseEvent(self, event):
-        items = self.items(event.pos())
-        if items and isinstance(items[0], QGraphicsItem):
-            QGraphicsView.mouseReleaseEvent(self, event)
-            return
-        if event.button() == Qt.MouseButton.RightButton:
-            self.rightButtonPressed = False
-            self.setCursor(Qt.ArrowCursor)
-            event.accept()
-
+    #
+    #     elif event.button() == Qt.MouseButton.RightButton:
+    #         self.rightButtonPressed = True
+    #         self.panStartX = event.x()
+    #         self.panStartY = event.y()
+    #         self.setCursor(Qt.ClosedHandCursor);
+    #         event.accept()
+    #
+    #     elif event.button() == Qt.MouseButton.MiddleButton:
+    #         self.centerOn(event.pos())
+    #
+    # def mouseMoveEvent(self, event):
+    #     items = self.items(event.pos())
+    #     if items and isinstance(items[0], QGraphicsItem):
+    #         QGraphicsView.mouseMoveEvent(self, event)
+    #         return
+    #     if self.rightButtonPressed:
+    #         self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - (event.x() - self.panStartX))
+    #         self.verticalScrollBar().setValue(self.verticalScrollBar().value() - (event.y() - self.panStartY))
+    #         self.panStartX = event.x()
+    #         self.panStartY = event.y()
+    #         event.accept()
+    #
+    #
+    # def mouseReleaseEvent(self, event):
+    #     items = self.items(event.pos())
+    #     if items and isinstance(items[0], QGraphicsItem):
+    #         QGraphicsView.mouseReleaseEvent(self, event)
+    #         return
+    #     if event.button() == Qt.MouseButton.RightButton:
+    #         self.rightButtonPressed = False
+    #         self.setCursor(Qt.ArrowCursor)
+    #         event.accept()
 
     def wheelEvent(self, event):
         self.scaleView(math.pow(2.0, -event.angleDelta().y() / 240.0))
-
 
     def scaleView(self, scaleFactor):
         factor = self.transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width()
