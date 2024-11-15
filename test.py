c45fbc5bd96addd0de71c0d1ffeb1c452b5b82c0
olo00 <olo00olo@protonmail.com>
2024-11-15 02:50:58 +0100

    a lot of changes

---
+++ test.py
@@ -0,0 +1,55 @@
+import numpy as np
+#
+# m = [1]
+# c = [[-1], [2], [1]]
+# u = [0, 1, 1]
+#
+# # print(list(np.add(m, np.matmul(u, c))))
+# a = 0
+# for x in range(len(c)):
+#     a += u[x] * c[x][0]
+#
+# print(a)
+a = "aaaAa"
+
+print(a.islower())
+# for x in range(3):
+#     for y in range(4):
+#         if y == 2:
+#             break
+# print(x, y)
+
+# a = np.array(a)
+# c = np.sum(np.where(a > 0, a, 0), axis=0)
+# print(c)
+
+# for x in range(len(a)):
+#     if
+
+# temp = []
+# temp1 = []
+# for x in range(len(b)):
+#     if b[x] == 0:
+#         for y in range(len(a)):
+#             if a[y][x] > 0:
+#                 temp1.append(0)
+#             else:
+#                 temp1.append(1)
+#         temp.append(temp1)
+#         temp1 = []
+#
+#
+# print(temp)
+#
+# uCap = [1, 1, 1, 1]
+# for x in range(len(temp)):
+#     # print(uCap, temp[x])
+#     uCap = list(np.logical_and(uCap, temp[x]))
+#     uCap = list(map(int, uCap))
+# print(uCap)
+#
+# kolumny = 5
+# macierz = [1] * kolumny
+#
+# print(macierz)
+
